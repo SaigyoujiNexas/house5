62c7b29414002f7d8c9a8a30c32b62c45f82d13b
SaigyoujiNexas <saigyoujinexas@gmail.com>
2023-06-05 14:52:32 +0800

    Initial commit

---
+++ target/classes/com/xupt/house/common/constant/CommonConstant.java
@@ -0,0 +1,27 @@
+package com.xupt.house.common.constant;
+
+/**
+ * 常量
+ */
+public interface CommonConstant {
+
+    /**
+     * 正常状态
+     */
+    Integer STATUS_NORMAL = 0;
+
+    /**
+     * 用户密码加盐的盐
+     */
+    String PASSWORD_SALT = "sens";
+
+    /**
+     * none
+     */
+    String NONE = "none";
+
+    String CONDITION = "condition";
+
+    String TOKEN = "90lj+hi4viyis1BDmkQw0g==";
+
+}
